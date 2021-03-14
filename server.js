bd142eaa1dbf5c0cadba3844ecb61a8888e01868
Geeth Charan <geeth.charan99@gmail.com>
2021-03-14 13:58:37 +0530

    Implement chef+shop+shopmenu + related functionality

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use('/api/chefauth', require('./routes/api/chefauth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/userauth', require('./routes/api/userauth'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/shop', require('./routes/api/shop'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-
