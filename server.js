db3be59e532ab955b7a32dee6869cd84dc230808
Shivankthakur <tshivsingh@gmail.com>
2021-03-26 14:47:53 +0530

    added logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const logger = require('./middleware/logger')
 
 const app = express();
 
@@ -8,6 +9,7 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
+app.use(logger)
 
 app.get('/', (req, res) => res.send('API Running'));
 
